04c4c4c35b0eff0e29f35b25277cfa88c6cdce85
FysixNI <50052151@serc.ac.uk>
2024-05-08 15:34:34 +0100

    Initial Commit

---
+++ src/Book.java
@@ -0,0 +1,21 @@
+public class Book
+{
+    private String title;
+    private String author;
+
+    public Book(String aTitle, String aAuthor)
+    {
+        this.title = aTitle;
+        this.author = aAuthor;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getAuthor()
+    {
+        return author;
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,21 @@
+public class Person
+{
+    private String name;
+    private int age;
+
+    public Person(String aName, int aAge)
+    {
+        this.name = aName;
+        this.age = aAge;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+}
